2a7831ac389de8a7af60f816f8de92caf6c5d1b1
CrazyCatCZ <loxtox74@gmail.com>
2020-10-26 22:16:40 +0100

    added superuser

--- graphql_django/settings.py
+++ graphql_django/settings.py
@@ -33,7 +33,7 @@ DATABASES = {
     }
 }
 
-#connect new database
+#connect new postgres database
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
