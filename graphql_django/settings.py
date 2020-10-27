ca91cbd8f7630b7454c7fde9b63b8d13dfcce9b6
CrazyCatCZ <loxtox74@gmail.com>
2020-10-27 11:58:14 +0100

    final edit admin path

--- graphql_django/settings.py
+++ graphql_django/settings.py
@@ -19,7 +19,7 @@ SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback')
 
-DEBUG = True
+DEBUG = False
 
 #database
 DATABASES = {
