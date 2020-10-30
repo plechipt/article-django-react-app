bbd11df348cc724c37f2bb6918c282d73cf3b6b9
Jakub Plecháč <loxtox74@gmail.com>
2020-10-30 20:03:00 +0100

    removed parentheses

--- users/api_profiles.py
+++ users/api_profiles.py
@@ -114,7 +114,7 @@ class UpdateUser(graphene.Mutation):
          message = 'This email already exists!'
 
       #if new_user or new_email includes forbidden characters from forbidden_chars variable
-      elif ([char for char in forbidden_chars (if char in new_user_and_new_email)] == []) != True or input.new_email.count('@') > 1 or '@' in input.new_user:
+      elif ([char for char in forbidden_chars if char in new_user_and_new_email] == []) != True or input.new_email.count('@') > 1 or '@' in input.new_user:
          message = 'Name or email must not include special characters!'
 
       else:
