30fa2c443152afe8b4b8383eebc818750e85fcb3
Jakub Plecháč <loxtox74@gmail.com>
2020-11-01 16:42:04 +0100

    responsive paginator

--- src/components/Posts/Posts/Pagination.js
+++ src/components/Posts/Posts/Pagination.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Pagination, Icon } from 'semantic-ui-react'
 
 const CustomPagination = ({ postsPerPage, totalPosts, currentPage, handlePaginationChange }) => {
--- src/components/Posts/Posts/Posts.js
+++ src/components/Posts/Posts/Posts.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useQuery, useMutation } from '@apollo/react-hooks'
+import { useMutation } from '@apollo/react-hooks'
 import { Form } from 'semantic-ui-react'
 
 import { POST_FILTER_MUTATION  } from '../../Api/post'
