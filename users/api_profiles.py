62563f6f079c7944f80ebf8c838e61a70f8b4ddd
Jakub Plecháč <loxtox74@gmail.com>
2020-11-05 16:00:29 +0100

    set limit of chars in username and email in update profile

--- users/api_profiles.py
+++ users/api_profiles.py
@@ -116,6 +116,12 @@ class UpdateUser(graphene.Mutation):
       input.new_email.count('@') > 1 or '@' in input.new_user or '.' in input.new_user):
          message = 'Name or email must not include special characters!'
 
+      elif len(input.new_user) >= 40:
+         message = 'Your new name has reached maximum of characters! (40 characters)'
+
+      elif len(input.new_email) >= 50:
+         message = 'Your new email has reached maximum of characters! (50 characters)'
+
       else:
          message = 'Success'
          user.username = input.new_user
