8d56961098dd9f1d5631485e1011eb609cc4b555
Jakub Plecháč <loxtox74@gmail.com>
2020-11-15 12:42:24 +0100

    add settings.py and urls.py

---
+++ graphql_django/urls.py
@@ -0,0 +1,20 @@
+import os
+from django.contrib import admin
+from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+from .schema import schema
+
+
+ADMIN_PATH = os.environ.get('ADMIN_PATH')
+
+urlpatterns = [
+    path(f'{ADMIN_PATH}/', admin.site.urls),
+    path(f'api/', csrf_exempt(GraphQLView.as_view(schema=schema, graphiql=False))),
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
