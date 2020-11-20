ac60be381109d5fff0b1a19c5b77258e6e45267e
Jakub Plecháč <loxtox74@gmail.com>
2020-11-20 12:35:53 +0100

    implemented refresh token

--- graphql_django/files/graphql.py
+++ graphql_django/files/graphql.py
@@ -14,8 +14,9 @@ AUTHENTICATION_BACKENDS = [
 
 GRAPHQL_JWT = {
     "JWT_VERIFY_EXPIRATION": True,
+    "JWT_LONG_RUNNING_REFRESH_TOKEN": True,
     'JWT_EXPIRATION_DELTA': timedelta(minutes=15),
-    "JWT_LONG_RUNNING_REFRESH_TOKEN": False,
+    'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=7),
     
     "JWT_ALLOW_ANY_CLASSES": [
         "graphql_auth.mutations.Register",
