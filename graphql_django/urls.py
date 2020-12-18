f2a066145a40bc2224373f331aeafd9bc35fece8
Jakub Plecháč <loxtox74@gmail.com>
2020-12-19 00:14:45 +0100

    special menu item

--- graphql_django/urls.py
+++ graphql_django/urls.py
@@ -1,22 +1,37 @@
 import os
-from django.contrib import admin
-from django.urls import path, re_path
+
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
-
+from django.contrib import admin
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseForbidden
+from django.urls import path, re_path
 from django.views.decorators.csrf import csrf_exempt
-from graphql_jwt.decorators import jwt_cookie
+from django.views.generic import TemplateView
 from graphene_django.views import GraphQLView
-
+from graphql_jwt.decorators import jwt_cookie
 from users.models import CustomUser
+
 from .schema import schema
 
-
 ADMIN_PATH = os.environ.get('ADMIN_PATH')
+API_KEY = os.environ.get('CUSTOM_API_KEY')
+
+class CustomGraphQLView(GraphQLView):
+    def dispatch(self, request, *args, **kwargs):
+        res = super(CustomGraphQLView, self).dispatch(request, *args, **kwargs)
+        passed_api_key = request.headers['Authorization']
+
+        if API_KEY != passed_api_key:
+            return HttpResponseForbidden()
+
+        else:
+            return res
+
+
 
 urlpatterns = [
     path(f'{ADMIN_PATH}/', admin.site.urls),
-    path('graphql/', jwt_cookie(GraphQLView.as_view(schema=schema, graphiql=False))),
+    path('graphql/', csrf_exempt(CustomGraphQLView.as_view(schema=schema, graphiql=True))),
     re_path('.*', TemplateView.as_view(template_name='index.html'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
