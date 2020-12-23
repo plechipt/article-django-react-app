52ad08f0140615a8fc96e6d7a30cab2eb36c190b
Jakub Plecháč <loxtox74@gmail.com>
2020-12-24 00:35:21 +0100

    changed api key environment

--- graphql_django/urls.py
+++ graphql_django/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.core.exceptions import PermissionDenied
-from django.http import HttpResponseForbidden
+from django.http import Http404, HttpResponseForbidden
 from django.urls import path, re_path
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
@@ -12,13 +12,23 @@ from graphene_django.views import GraphQLView
 from graphql_jwt.decorators import jwt_cookie
 from users.models import CustomUser
 
+from . import settings
 from .schema import schema
 
 ADMIN_PATH = os.environ.get('ADMIN_PATH')
-API_KEY = os.environ.get('CUSTOM_API_KEY')
+API_KEY = os.environ.get('REACT_APP_API_KEY')
+
+local_base_url = 'http://127.0.0.1:8000'
+local_base_url2 = 'http://localhost:3000'
+heroku_base_url = 'https://article-django-react-app.herokuapp.com'
+
+staff_urls = [
+    f'{local_base_url}/graphql/', f'{local_base_url2}/graphql/', f'{heroku_base_url}/graphql/',
+]
 
 class CustomGraphQLView(GraphQLView):
     def dispatch(self, request, *args, **kwargs):
+        def dispatch(self, request, *args, **kwargs):
         res = super(CustomGraphQLView, self).dispatch(request, *args, **kwargs)
         
         # If Authorization is not passed to headers -> return 403
@@ -31,7 +41,8 @@ class CustomGraphQLView(GraphQLView):
         if API_KEY != passed_api_key:
             return HttpResponseForbidden()
         else:
-            return res
+            return ress
+
 
 urlpatterns = [
     path(f'{ADMIN_PATH}/', admin.site.urls),
