e5deaca806e190af764dbcb05d1d0338c2942abc
Jakub Plecháč <loxtox74@gmail.com>
2020-12-24 11:16:17 +0100

    grapihql set to False

--- graphql_django/urls.py
+++ graphql_django/urls.py
@@ -35,9 +35,8 @@ class CustomGraphQLView(GraphQLView):
             return res
 
 
-
 urlpatterns = [
     path(f'{ADMIN_PATH}/', admin.site.urls),
-    path('graphql/', jwt_cookie(CustomGraphQLView.as_view(schema=schema, graphiql=True))),
+    path('graphql/', jwt_cookie(CustomGraphQLView.as_view(schema=schema, graphiql=False))),
     re_path('.*', TemplateView.as_view(template_name='index.html'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
