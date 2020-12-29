119dfef52ce74b0e560628930ef259200d9e5239
Jakub Plecháč <loxtox74@gmail.com>
2020-12-29 11:07:10 +0100

    handle on expired jwt token on backend

--- graphql_django/urls.py
+++ graphql_django/urls.py
@@ -1,10 +1,11 @@
 import os
+import time
 
-from django.conf import settings
+import jwt
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.core.exceptions import PermissionDenied
-from django.http import Http404, HttpResponseForbidden
+from django.http import Http404, HttpResponse, HttpResponseForbidden
 from django.urls import path, re_path
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
@@ -21,7 +22,17 @@ API_KEY = os.environ.get('REACT_APP_API_KEY')
 class CustomGraphQLView(GraphQLView):
     def dispatch(self, request, *args, **kwargs):
         res = super(CustomGraphQLView, self).dispatch(request, *args, **kwargs)
-        
+        cookies = request.COOKIES
+        access_token = cookies.get('accessToken')
+        current_time = round(time.time())
+
+        if access_token:
+            decoded_jwt = jwt.decode(access_token, settings.SECRET_KEY, algorithms=["HS256"])
+            expiration_time = decoded_jwt['exp']
+
+            if (expiration_time - current_time) <= 0:
+                return HttpResponse('Unauthorized', status=401)
+
         # If Authorization is not passed to headers -> return 403
         try:
             passed_api_key = request.headers['Authorization']
@@ -31,8 +42,8 @@ class CustomGraphQLView(GraphQLView):
         # If passed API_KEY is incorrect -> return 403
         if API_KEY != passed_api_key:
             return HttpResponseForbidden()
-        else:
-            return res
+
+        return res
 
 
 urlpatterns = [
