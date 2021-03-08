fb45871d5248c2f9c35bb7bced148d37b439f0bd
Jakub Plecháč <loxtox74@gmail.com>
2021-03-08 22:12:39 +0100

    edited MIDDLEWARE

--- graphql_django/files/basic.py
+++ graphql_django/files/basic.py
@@ -29,7 +29,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
